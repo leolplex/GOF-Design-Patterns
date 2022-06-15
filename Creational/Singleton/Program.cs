63924e88765d65fc8a0b8e9a8b3c877b07e73c9d
Daniel Romero <leogolplex@gmail.com>
2022-06-15 16:37:27 -0500

    Add singleton pattern

--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Conceptual.NonThreadSafe;
+using Conceptual.ThreadSafe;
+
+Singleton s1 = Singleton.GetInstance();
+Singleton s2 = Singleton.GetInstance();
+
+if (s1 == s2)
+{
+    Console.WriteLine("Singleton works, both variables contain the same instance.");
+}
+else
+{
+    Console.WriteLine("Singleton failed, variables contain different instances.");
+}
+
+
+// The client code.
+
+Console.WriteLine(
+    "{0}\n{1}\n\n{2}\n",
+    "If you see the same value, then singleton was reused (yay!)",
+    "If you see different values, then 2 singletons were created (booo!!)",
+    "RESULT:"
+);
+
+Thread process1 = new Thread(() =>
+{
+    TestSingleton("FOO");
+});
+Thread process2 = new Thread(() =>
+{
+    TestSingleton("BAR");
+});
+
+process1.Start();
+process2.Start();
+
+process1.Join();
+process2.Join();
+
+
+static void TestSingleton(string value)
+{
+    SingletonThreadSafe singleton = SingletonThreadSafe.GetInstance(value);
+    Console.WriteLine(singleton.Value);
+}
--- README.md
+++ README.md
@@ -14,3 +14,7 @@ Builder is a creational design pattern that lets you construct complex objects s
 ### Prototype
 
 Prototype is a creational design pattern that lets you copy existing objects without making your code dependent on their classes.
+
+### Singleton
+
+Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.
